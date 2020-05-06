d14fa882afc23f85ca88145e1d68f7f1c6a43cb8
Mateusz Dobija <mateusz.dobija.rce@gmail.com>
2020-05-06 20:45:41 +0200

    Updates

---
+++ precache-manifest.509ef6766653d753f1a64491a0f6e333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4011030b88c94d76a15e36d2a62d0a1",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "b4ae1d5183ddadb21e6f",
+    "url": "/To-Do-App-in-React/static/css/main.2f4e3038.chunk.css"
+  },
+  {
+    "revision": "75a784b766a367e61706",
+    "url": "/To-Do-App-in-React/static/js/2.ee427382.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App-in-React/static/js/2.ee427382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ae1d5183ddadb21e6f",
+    "url": "/To-Do-App-in-React/static/js/main.2730dee4.chunk.js"
+  },
+  {
+    "revision": "b6429c8793e63c50de7d",
+    "url": "/To-Do-App-in-React/static/js/runtime-main.c7b7acd1.js"
+  }
+]);
